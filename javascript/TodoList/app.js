aeb971d3d84985435f96f066111b232cecd09e8c
eternrust <eternrust@dsm.hs.kr>
2022-05-22 23:32:50 +0900

    5-22 commit

--- javascript/TodoList/app.js
+++ javascript/TodoList/app.js
@@ -1,13 +1,17 @@
 let todolist = [];
-let cnt = 0
 let id = 0;
 const List = document.getElementById('List');
-
+const TodoDel = document.querySelector('.Del');
+const todoInput = document.querySelector('.input');
 function NewValue(e){
     let text = document.getElementById('txt').value;
     if(e.keyCode == 13){
+        if(text){
         AddTodos(text);
         document.getElementById('txt').value='';
+        } else {
+            alert('값을 입력해주세요!');
+        }
     }
 }
 
@@ -25,8 +29,34 @@ const AddTodos = (text) => {
     update();
 }
 
+const Checking = (todoId) => {
+    const todo = getAllTodos().map(todo => todo.id === todoId ? {...todo, completed:!todo.completed} : todo);
+    setTodos(todo);
+    update();
+}
+
+const Delete = (todoId) => {
+    const todo = getAllTodos().filter(todo => todo.id !== todoId);
+    setTodos(todo);
+    update();
+}
+
+const resetText = (e, todoId) => {
+    const todoElem = e.target;
+    const inputText = e.target.innerText;
+    const todoItemElem = todoElem.parentNode;
+    const inputElem = document.createElement('input');
+    inputElem.value = inputText;
+    inputElem.classList.add('edit');
+
+    todoItemElem.appendChild(inputElem);
+}
+
 const update = () => {
     List.innerHTML = null;
+    const todospan = TodoDel.querySelector('span');
+    const todolength = getAllTodos().filter(todo => todo.completed == false).length;
+    todospan.innerHTML = todolength + ' Items left';
     const allTodos=getAllTodos();
     allTodos.forEach(todos => {
         const todoDiv = document.createElement('div');
@@ -40,10 +70,12 @@ const update = () => {
         const todoText = document.createElement('div');
         todoText.classList.add('todo');
         todoText.innerHTML=todos.content;
+        todoText.addEventListener('dblclick', (event) => resetText(event, todos.id));
 
         const todoDel = document.createElement('button');
         todoDel.classList.add('X');
         todoDel.innerHTML = 'X';
+        todoDel.addEventListener('click', () => Delete(todos.id));
 
         if(todos.completed){
             todoDiv.classList.add('checked');
@@ -57,12 +89,16 @@ const update = () => {
         List.appendChild(todoDiv);
     })
 }
-const checking = (Id) => {
-    const todo = todolist.find(callback[id=Id]);
-    if(todo.completed){
-        todo.completed = false;
-    } else {
-        todo.completed = true;
+
+TodoDel.addEventListener('click',(e) => {
+    if(e.target.id=='All'){
+        TodoDel.setAttribute("data-id",'1');
+    } else if(e.target.id=='Active'){
+        TodoDel.setAttribute("data-id",'2');
+    } else if(e.target.id=='Completed'){
+        TodoDel.setAttribute("data-id",'3');
+    } else if(e.target.id=='Clear'){
+        todolist=[];
     }
     update();
-}
+})
