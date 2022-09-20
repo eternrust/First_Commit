cf2f33c570b01d127d7071866009b82176fae380
eternrust <eternrust@dsm.hs.kr>
2022-09-20 23:30:03 +0900

    add audio_Time

--- javascript/music_radio/app.js
+++ javascript/music_radio/app.js
@@ -14,13 +14,7 @@ const handleFiles = (e) => {
   fileReader.onload = function () {
     // audio.src = fileReader.result;
     MyAudio.src = fileReader.result;
-    if(!pause) {
-      MyAudio.play();
-      MyAudio.currentTime = "0";
-      MyAudio.textTracks.onaddtrack = (event) => {
-        console.log(`Text track: ${event.track.label} added`);
-      };
-    }
+    MyAudio.addEventListener('loadedmetadata', () => Audio_start());
   };
 };
 
@@ -36,8 +30,32 @@ const pause_click = () => {
   }
 }
 
+const Audio_start = () => {
+  if(!pause) {
+    MyAudio.play();
+    MyAudio.currentTime = 0;
+  }
+
+  let b_minute = Math.floor(MyAudio.duration/60);
+  let b_second = Math.floor(MyAudio.duration%60);
+  if(b_second < 10) {
+    b_second = `0${b_second}`;
+  }
+
+  setInterval(() => {
+    let f_minute = Math.floor(MyAudio.currentTime/60);
+  let f_second = Math.floor(MyAudio.currentTime%60);
+  if(f_second < 10) {
+    f_second = `0${f_second}`;
+  }
+
+  time_text.innerText = `${f_minute} : ${f_second} / ${b_minute} : ${b_second}`;
+  },1000)
+}
+
 pause_btn.innerText = "▶︎";
 pause_btn.addEventListener("click", () => pause_click());
 
+time_text.innerText = '0 : 00 / 0 : 00';
 
 fileInput.addEventListener("change", handleFiles);
