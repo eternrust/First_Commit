3b98340eff2fdfdf092ccbb36ac4e7693665beb7
eternrust <eternrust@dsm.hs.kr>
2022-09-23 23:34:49 +0900

    adding drog&drop...

--- javascript/music_radio/app.js
+++ javascript/music_radio/app.js
@@ -11,6 +11,16 @@ let pause = false;
 //오디오 객체 불러오기
 let MyAudio = new Audio();
 
+// 클릭 중인가?
+let clicked = false;
+
+// 마우스를 움직이는 중인가?
+let moving = false;
+
+// bar_btn를 클릭했을 때 X좌표
+let bar_btn_x = null;
+
+
 // 음악 파일 불러오기
 const handleFiles = (e) => {
   const selectedFile = [...fileInput.files];
@@ -19,6 +29,7 @@ const handleFiles = (e) => {
 
   fileReader.onload = function () {
     // audio.src = fileReader.result;
+    pause = true;
     MyAudio.src = fileReader.result;
     MyAudio.addEventListener('loadedmetadata', () => Audio_start());
     bar_btn.style.backgroundColor = '#222';
@@ -27,45 +38,53 @@ const handleFiles = (e) => {
 
 // 음악 시작(정지) 버튼 눌렀을 때
 const pause_click = () => {
-  if(pause) {
-    if(MyAudio.src == undefined) {
+  if (pause) {
+    if (MyAudio.src !== undefined) {
       Audio_start();
     }
-    pause_btn.innerText = "▶︎";
+    pause_btn.innerText = "❚❚";
     pause = false;
   } else {
-    if(MyAudio.src == undefined) {
+    if (MyAudio.src !== undefined) {
       MyAudio.pause();
     }
-    pause_btn.innerText = "❚❚";
+    pause_btn.innerText = "▶︎";
     pause = true;
   }
 }
 
-// 음악 시작
-const Audio_start = () => {
-  MyAudio.play();
+const set_time = () => {
+  let f_minute = Math.floor(MyAudio.currentTime / 60);
+  let f_second = Math.floor(MyAudio.currentTime % 60);
+  if (f_second < 10) {
+    f_second = `0${f_second}`;
+  }
 
-  let b_minute = Math.floor(MyAudio.duration/60);
-  let b_second = Math.floor(MyAudio.duration%60);
-  if(b_second < 10) {
+  let b_minute = Math.floor(MyAudio.duration / 60);
+  let b_second = Math.floor(MyAudio.duration % 60);
+  if (b_second < 10) {
     b_second = `0${b_second}`;
   }
 
+  time_text.innerText = `${f_minute} : ${f_second} / ${b_minute} : ${b_second}`;
+}
+
+// 음악 시작
+const Audio_start = () => {
+  pause_btn.innerText = "❚❚";
+  MyAudio.play();
+  pause = false;
+
   const Time_view = setInterval(() => {
-    let f_minute = Math.floor(MyAudio.currentTime/60);
-    let f_second = Math.floor(MyAudio.currentTime%60);
-    if(f_second < 10) {
-      f_second = `0${f_second}`;
-    }
-    // 오류!
+    set_time();
     bar_set();
 
-    time_text.innerText = `${f_minute} : ${f_second} / ${b_minute} : ${b_second}`;
-    if(pause) {
+    if (pause || MyAudio.currentTime == MyAudio.duration) {
       clearInterval(Time_view);
+      pause_btn.innerText = "▶︎";
+      pause = true;
     }
-  },1000)
+  }, 1000)
 }
 
 // 절대 좌표 구하는 함수(왼쪽)
@@ -75,13 +94,33 @@ const getAbsoluteLeft = (element) => {
 
 // 음악 재생 위치 결정하기(미완성)
 const bar_set = () => {
-  const parentAbsoluteLeft = getAbsoluteLeft(audio_bar); 
-  const AbsoluteLeft = getAbsoluteLeft(bar_btn);
+  // const parentAbsoluteLeft = getAbsoluteLeft(audio_bar); 
+  // const AbsoluteLeft = getAbsoluteLeft(bar_btn);
+  // // bar_btn의 현재 상대 좌표
+  // const relativeLeft =  AbsoluteLeft - parentAbsoluteLeft;
+  // const elementPercent = relativeLeft / 175 * 100;
+  const percentTime = MyAudio.currentTime / MyAudio.duration * 100;
+  bar_btn.style.marginLeft = `${percentTime / 100 * 175}px`;
+}
+
+// audio_bar 클릭 위치를 재생 위치로 변환
+const bar_click = (e) => {
+  const parentAbsoluteLeft = getAbsoluteLeft(audio_bar);
+  let x = e.clientX;
   // bar_btn의 현재 상대 좌표
-  const relativeLeft =  AbsoluteLeft - parentAbsoluteLeft;
-  const percentTime =  MyAudio.currentTime / MyAudio.duration * 100;
-  const elementPercent = relativeLeft / 175 * 100;
-  bar_btn.style.marginLeft = Math.floor(percentTime / 100 * 175);
+  const relativeLeft = x - parentAbsoluteLeft;
+  MyAudio.currentTime = (relativeLeft - 12.5) / 175 * MyAudio.duration
+
+  set_time();
+  bar_set();
+}
+
+// bar_btn의 Drog&Drop(미완)
+const drog_drop = (e) => {
+  let x = e.clientX;
+  let relativeLeft;
+
+  MyAudio.currentTime = (relativeLeft - 12.5) / 175 * MyAudio.duration
 }
 
 // 초기 기본 시작(정지) 버튼 설정
@@ -91,5 +130,12 @@ pause_btn.addEventListener("click", () => pause_click());
 // 초기 기본 초 설정
 time_text.innerText = '0 : 00 / 0 : 00';
 
-// 음악 파일 불러오기
+// audio_bar를 클릭했을 때
+audio_bar.addEventListener("click", (event) => { clicked = true; moving = false; bar_click(event); });
+// bar_btn 드래그 앤 드롭
+bar_btn.addEventListener('mousedown', () =>   bar_btn_x = e.clientX);
+// audio_bar.addEventListener('mousemove', (event) => { moving = true; bar_click(event); });
+// audio_bar.addEventListener('mouseup', () => { clicked = false; moving = false;);
+
+  // 음악 파일 불러오기
 fileInput.addEventListener("change", handleFiles);
