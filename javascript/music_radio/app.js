75f8eecacb85bc2c454167dc68687717c6730da5
eternrust <eternrust@dsm.hs.kr>
2022-09-30 23:40:20 +0900

    볼륨 조절을 만들기 위한 초기 설정

--- javascript/music_radio/app.js
+++ javascript/music_radio/app.js
@@ -1,22 +1,26 @@
 const fileInput = document.getElementById("fileUpload");
 // const audio = document.getElementById("myAudio");
-const pause_btn = document.getElementById("Audio_pause");
-const time_text = document.getElementById("Audio_time");
-const audio_bar = document.getElementById("Audio_bar");
-const bar_btn = document.getElementById("Audio_bar_button");
+const Pause_btn = document.getElementById("Audio_pause");
+const Time_text = document.getElementById("Audio_time");
+const Audio_bar = document.getElementById("Audio_bar");
+const Audio_bar_btn = document.getElementById("Audio_bar_button");
 const Audio_sound = document.getElementById("Audio_sound");
-
-// 음악을 멈추지 않았다 == false │ 음악이 멈췄다 == true
-let pause = false;
+const Sound_box = document.getElementById("Sound_box");
+const Sound_volume = document.getElementById("Sound_volume");
+const Sound_bar = document.getElementById("Sound_bar");
+const Sound_bar_btn = document.getElementById("Sound_bar_button");
 
 //오디오 객체 불러오기
 let MyAudio = new Audio();
+
+// 음악을 멈추지 않았다 == false │ 음악이 멈췄다 == true
+let pause = false;
 
 // 마우스 이동을 시작했는가? (Drog&Drop)
 let isDragging = false;
 
 // 볼륨은 몇인가?
-let Volume = 50;
+let volume = 50;
 
 // 음악 파일 불러오기
 const handleFiles = (e) => {
@@ -29,10 +33,10 @@ const handleFiles = (e) => {
     pause = true;
     MyAudio.src = fileReader.result;
     MyAudio.addEventListener('loadedmetadata', () => Audio_start());
-    bar_btn.style.backgroundColor = '#222';
-    pause_btn.style.filter= 'opacity(1) drop-shadow(0 0 0 #000)';
+    Audio_bar_btn.style.backgroundColor = '#222';
+    Pause_btn.style.filter= 'opacity(1) drop-shadow(0 0 0 #000)';
     Audio_sound.style.filter= 'opacity(1) drop-shadow(0 0 0 #000)';
-    bar_btn.style.display = 'block';
+    Audio_bar_btn.style.display = 'block';
   };
 };
 
@@ -42,13 +46,13 @@ const pause_click = () => {
     if (MyAudio.src !== undefined) {
       Audio_start();
     }
-    pause_btn.innerText = "❚❚";
+    Pause_btn.innerText = "❚❚";
     pause = false;
   } else {
     if (MyAudio.src !== undefined) {
       MyAudio.pause();
     }
-    pause_btn.innerText = "▶︎";
+    Pause_btn.innerText = "▶︎";
     pause = true;
   }
 }
@@ -66,25 +70,30 @@ const set_time = () => {
     b_second = `0${b_second}`;
   }
 
-  time_text.innerText = `${f_minute} : ${f_second} / ${b_minute} : ${b_second}`;
+  Time_text.innerText = `${f_minute} : ${f_second} / ${b_minute} : ${b_second}`;
 }
 
 // 음악 시작
 const Audio_start = () => {
-  pause_btn.innerText = "❚❚";
+  Pause_btn.innerText = "❚❚";
   MyAudio.play();
   pause = false;
 
   const Time_view = setInterval(() => {
     set_time();
-    bar_set();
+    bar_set_time();
 
     if (pause || MyAudio.currentTime == MyAudio.duration) {
       clearInterval(Time_view);
-      pause_btn.innerText = "▶︎";
+      Pause_btn.innerText = "▶︎";
       pause = true;
     }
   }, 1000)
+}
+
+// 절대 좌표 구하는 함수(위쪽)
+const getAbsoluteTop = (element) => {
+  return window.pageYOffset + element.getBoundingClientRect().top;
 }
 
 // 절대 좌표 구하는 함수(왼쪽)
@@ -93,23 +102,23 @@ const getAbsoluteLeft = (element) => {
 }
 
 // 음악 재생 위치 결정하기(미완성)
-const bar_set = () => {
-  // const parentAbsoluteLeft = getAbsoluteLeft(audio_bar); 
-  // const AbsoluteLeft = getAbsoluteLeft(bar_btn);
+const bar_set_time = () => {
+  // const parentAbsoluteLeft = getAbsoluteLeft(Audio_bar); 
+  // const AbsoluteLeft = getAbsoluteLeft(Audio_bar_btn);
   // // bar_btn의 현재 상대 좌표
   // const relativeLeft =  AbsoluteLeft - parentAbsoluteLeft;
   // const elementPercent = relativeLeft / 175 * 100;
   const percentTime = MyAudio.currentTime / MyAudio.duration * 100;
-  bar_btn.style.marginLeft = `${percentTime / 100 * 130}px`;
+  Audio_bar_btn.style.marginLeft = `${percentTime / 100 * 130}px`;
 }
 
-// audio_bar 클릭 위치를 재생 위치로 변환
+// Audio_bar 클릭 위치를 재생 위치로 변환
 const bar_click = (e) => {
   if(isDragging && !pause) {
     if (MyAudio.src !== undefined) {
       MyAudio.pause();
     }
-    pause_btn.innerText = "▶︎";
+    Pause_btn.innerText = "▶︎";
     pause = true;
   }
   let x = e.clientX;
@@ -118,23 +127,40 @@ const bar_click = (e) => {
   MyAudio.currentTime = (relativeLeft - 10) / 130 * MyAudio.duration
 
   set_time();
-  bar_set();
+  bar_set_time();
 }
 
+// 볼륨 이미지 변경
+const set_volume = () => {
+  if(volume == 0) {
+    Audio_sound.src = './sound_off.png';
+  } else {
+    Audio_sound.src = './sound_on.png';
+  }
+}
+
+const bar_set_sound = () => {
+  MyAudio.volume = volume / 100;
+
+}
+
+bar_set_sound();
+
 // 초기 기본 시작(정지) 버튼 설정
-pause_btn.innerText = "▶︎";
-pause_btn.addEventListener("click", () => pause_click());
+Pause_btn.innerText = "▶︎";
+Pause_btn.addEventListener("click", () => pause_click());
 
 // 초기 기본 초 설정
-time_text.innerText = '0 : 00 / 0 : 00';
+Time_text.innerText = '0 : 00 / 0 : 00';
 
 // audio_bar의 절대위치
-const parentAbsoluteLeft = getAbsoluteLeft(audio_bar);
+const parentAbsoluteLeft = getAbsoluteLeft(Audio_bar);
+const parentAbsoluteTop = getAbsoluteTop(Audio_sound);
 
 // audio_bar를 클릭했을 때
-audio_bar.addEventListener("click", (event) => { clicked = true; moving = false; bar_click(event); });
-// bar_btn 드래그 앤 드롭
-bar_btn.addEventListener('mousedown', () =>   {isDragging = true;});
+Audio_bar.addEventListener("click", (event) => { clicked = true; moving = false; bar_click(event); });
+// Audio_bar_btn 드래그 앤 드롭
+Audio_bar_btn.addEventListener('mousedown', () =>   {isDragging = true;});
 document.addEventListener('mousemove', (event) => { if(isDragging) {bar_click(event);} });
 document.addEventListener('mouseup', () => { if(isDragging) { Audio_start(); } isDragging = false; });
 
